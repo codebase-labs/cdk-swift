a3f49bb92f29590f03ed56a65b194aaf28d7c625
Paul Young <84700+paulyoung@users.noreply.github.com>
2022-11-29 00:20:53 -0800

    Add wrapper around wasm-snip

--- flake.nix
+++ flake.nix
@@ -106,26 +106,106 @@
               pkgs.libiconv
             ];
           };
-        in
-        {
-          checks = {
-            # inherit ;
-          };
 
-          packages = {
-            # inherit ;
-          };
-
-          # defaultPackage = ;
-
-          devShell = pkgs.mkShell {
-            inputsFrom = builtins.attrValues self.checks;
-            nativeBuildInputs = with pkgs; [
-              dfinitySdk
+          wasm-snip = craneLib.buildPackage {
+            src = pkgs.stdenv.mkDerivation {
+              name = "wasm-snip-src";
+              src = pkgs.fetchCrate {
+                pname = "wasm-snip";
+                version = "0.4.0";
+                sha256 = "sha256-+oThqcy3H4/s2T+Uw0V/nnVzx7SW5xPghbaJxoub7yc=";
+              };
+              installPhase = ''
+                cp -R --preserve=mode,timestamps . $out
+              '';
+            };
+            doCheck = false;
+            nativeBuildInputs = [
+              pkgs.libiconv
             ];
-            shellHook = ''
-              export PATH=/Library/Developer/Toolchains/swift-wasm-5.6.0-RELEASE.xctoolchain/usr/bin:"$PATH";
-            '';
           };
-        });
+
+          # wasm-snip-wasi input.wasm --output output.wasm
+          wasm-snip-wasi = pkgs.runCommand "wasm-snip-wasi" {
+            buildInputs = [
+              pkgs.makeWrapper
+            ];
+          } ''
+              makeWrapper ${wasm-snip}/bin/wasm-snip $out/bin/wasm-snip-wasi \
+                --append-flags "\
+                  __imported_wasi_snapshot_preview1_args_get \
+                  __imported_wasi_snapshot_preview1_args_sizes_get \
+                  __imported_wasi_snapshot_preview1_clock_res_get \
+                  __imported_wasi_snapshot_preview1_clock_time_get \
+                  __imported_wasi_snapshot_preview1_environ_get \
+                  __imported_wasi_snapshot_preview1_environ_sizes_get \
+                  __imported_wasi_snapshot_preview1_fd_advise \
+                  __imported_wasi_snapshot_preview1_fd_allocate \
+                  __imported_wasi_snapshot_preview1_fd_close \
+                  __imported_wasi_snapshot_preview1_fd_datasync \
+                  __imported_wasi_snapshot_preview1_fd_fdstat_get \
+                  __imported_wasi_snapshot_preview1_fd_fdstat_set_flags \
+                  __imported_wasi_snapshot_preview1_fd_fdstat_set_rights \
+                  __imported_wasi_snapshot_preview1_fd_filestat_get \
+                  __imported_wasi_snapshot_preview1_fd_filestat_set_size \
+                  __imported_wasi_snapshot_preview1_fd_filestat_set_times \
+                  __imported_wasi_snapshot_preview1_fd_pread \
+                  __imported_wasi_snapshot_preview1_fd_prestat_dir_name \
+                  __imported_wasi_snapshot_preview1_fd_prestat_get \
+                  __imported_wasi_snapshot_preview1_fd_pwrite \
+                  __imported_wasi_snapshot_preview1_fd_read \
+                  __imported_wasi_snapshot_preview1_fd_readdir \
+                  __imported_wasi_snapshot_preview1_fd_renumber \
+                  __imported_wasi_snapshot_preview1_fd_seek \
+                  __imported_wasi_snapshot_preview1_fd_sync \
+                  __imported_wasi_snapshot_preview1_fd_tell \
+                  __imported_wasi_snapshot_preview1_fd_write \
+                  __imported_wasi_snapshot_preview1_path_create_directory \
+                  __imported_wasi_snapshot_preview1_path_filestat_get \
+                  __imported_wasi_snapshot_preview1_path_filestat_set_times \
+                  __imported_wasi_snapshot_preview1_path_link \
+                  __imported_wasi_snapshot_preview1_path_open \
+                  __imported_wasi_snapshot_preview1_path_readlink \
+                  __imported_wasi_snapshot_preview1_path_remove_directory \
+                  __imported_wasi_snapshot_preview1_path_rename \
+                  __imported_wasi_snapshot_preview1_path_symlink \
+                  __imported_wasi_snapshot_preview1_path_unlink_file \
+                  __imported_wasi_snapshot_preview1_poll_oneoff \
+                  __imported_wasi_snapshot_preview1_proc_exit \
+                  __imported_wasi_snapshot_preview1_proc_raise \
+                  __imported_wasi_snapshot_preview1_random_get \
+                  __imported_wasi_snapshot_preview1_sched_yield \
+                  __imported_wasi_snapshot_preview1_sock_recv \
+                  __imported_wasi_snapshot_preview1_sock_send \
+                  __imported_wasi_snapshot_preview1_sock_shutdown \
+                "
+          '';
+        in
+          {
+            checks = {
+              # inherit ;
+            };
+
+            apps = {
+              # inherit ;
+            };
+
+            packages = {
+              # inherit ;
+            };
+
+            # defaultPackage = ;
+
+            devShell = pkgs.mkShell {
+              inputsFrom = builtins.attrValues self.checks;
+              nativeBuildInputs = [
+                dfinitySdk
+                wasm-snip-wasi
+              ];
+              shellHook = ''
+                export PATH=/Library/Developer/Toolchains/swift-wasm-5.6.0-RELEASE.xctoolchain/usr/bin:"$PATH";
+              '';
+            };
+          }
+      );
 }
